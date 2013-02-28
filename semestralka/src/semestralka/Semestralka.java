a3b7ce9e4679553b7dc4092fdd718a3ed5ef3f3f
Michal <Michal@10.0.0.33>
2013-02-28 11:15:01 +0100

    1. - vytvoření projektu

--- semestralka/src/semestralka/Semestralka.java
+++ semestralka/src/semestralka/Semestralka.java
@@ -16,5 +16,7 @@ public class Semestralka {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+        
+        System.out.println("sda");
     }
 }
