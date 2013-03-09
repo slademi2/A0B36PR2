e0e3a5dab67f80082bb34427ee84c88328d57967
Michal <Michal@Piskle-THINK>
2013-03-09 12:19:33 +0100

    pokusy s grafikou grafiku

---
+++ semestralka/src/semestralka/Okno.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semestralka;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Michal
+ */
+public class Okno extends JFrame {
+    JButton but = new JButton("Novy");
+    JButton b = new JButton("Ahoj");
+    public Okno(){
+        GridBagLayout gb1 = new GridBagLayout();// mrizkovany layout
+        this.setLayout(gb1);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gb1.setConstraints(but, gbc);// vlozeni komponenty !!
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    void set (){
+        
+        this.pack();
+        this.setVisible(true);
+    }
+    
+}
--- semestralka/src/semestralka/Semestralka.java
+++ semestralka/src/semestralka/Semestralka.java
@@ -9,12 +9,14 @@ package semestralka;
  * @author Michal
  */
 public class Semestralka {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("novypokus o komit");
+        Okno ok1 = new Okno();
+        ok1.set();
     }
 }
